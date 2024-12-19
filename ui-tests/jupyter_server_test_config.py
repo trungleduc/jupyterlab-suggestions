c3140e9ba47fa1facb92c99d21caecd7c53c8a05
Duc Trung Le <leductrungxf@gmail.com>
2024-12-19 12:31:07 +0100

    Activate collaboration for rtc backend

--- ui-tests/jupyter_server_test_config.py
+++ ui-tests/jupyter_server_test_config.py
@@ -4,10 +4,11 @@
 opens the server to the world and provide access to JupyterLab
 JavaScript objects through the global window variable.
 """
-
+import os
 from jupyterlab.galata import configure_jupyter_server
 
+backend = os.getenv("BACKEND", None)
 configure_jupyter_server(c)  # noqa F821
-c.LabApp.collaborative = True  # noqa F821
+c.LabApp.collaborative = backend == 'rtc'  # noqa F821
 # Uncomment to set server log level to debug level
 # c.ServerApp.log_level = "DEBUG"
